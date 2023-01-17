826e9dd64f4d8bbf3715ea6ef31b2b3ce873dbe6
dr-aheydari <aliheydari@ucdavis.edu>
2023-01-16 19:08:49 -0800

    staging utilities move to a separate directory + reformatted docstrings

---
+++ NACT/utilities/__init__.py
@@ -0,0 +1,6 @@
+"""NACT's second layer import for utilies."""
+from .model_evaluation import *
+from .model_handling import *
+from .parameter_counting import *
+from .pickling import *
+from .weight_initialization import *
